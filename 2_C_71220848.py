3dce15c25b689471833f4c2394ce85f0f8d328e8
Priesma <117843377+Priesma@users.noreply.github.com>
2022-11-17 16:22:24 +0700

    Add files via upload

---
+++ 2_C_71220848.py
@@ -0,0 +1,9 @@
+#case soal 2
+cepat = int(input("Masukan kecepatan tempuh :"))
+waktu = int(input("Masukan waktu yang diperlukan :"))
+#rumus
+bensin=cepat*waktu/10
+biaya=bensin*15000
+#hasil jumlah akhir
+print("Teman Anda mengisi bensin sebanyak",bensin,"liter")
+print("Biaya yang dikeluarkan untuk mengisi bensin adalah Rp.",biaya)
